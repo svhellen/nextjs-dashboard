f577579e530d6e4051c30444f15d840f268e5072
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-04-18 19:35:17 -0300

    Add customers page content

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,26 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
- 
+
 export const metadata: Metadata = {
-  title: 'Costumers',
+  title: 'Customers',
 };
 
-export default function Costumers() {
-    return <p>Costumers Page</p>;
-  }
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'Create Invoice',
+  title: 'Login',
 };
  
 export default function LoginPage() {
