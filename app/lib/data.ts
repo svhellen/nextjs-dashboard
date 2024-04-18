02aaaa9a0ec2a6c6c066be348551bb877ba427e2
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-04-17 21:58:18 -0300

    Learn Next.js chapters 11 to 14
    Adding search and pagination, mutating data, handling erros, improving accessibility

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,8 +12,6 @@ import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
 
   try {
